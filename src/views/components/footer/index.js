f70417b1579438527a85edf14c873b25956455e0
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2017-05-05 17:40:30 +0300

    Make footer social buttons active.

--- src/views/components/footer/index.js
+++ src/views/components/footer/index.js
@@ -1,3 +1,4 @@
+import { $ } from 'backbone';
 import Epoxy from 'backbone.epoxy';
 import template from './Footer.jade';
 import './Footer.scss';
@@ -6,9 +7,12 @@ export default Epoxy.View.extend({
   template,
   className: 'footer',
   events: {
-
+    'click [data-js-href]': 'openSocial',
   },
   initialize() {
     this.renderTemplate();
+  },
+  openSocial(e) {
+    window.open($(e.currentTarget).attr('data-js-href'));
   },
 });
