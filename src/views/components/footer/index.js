3d18e78437b021cae05209b1f198d846b4ae3d04
Artsiom_Yeliseyenka <artsiom_yeliseyenka@epam.com>
2017-06-14 15:17:18 +0300

    Text fixes on landing & added EPAM logo into footer.

--- src/views/components/footer/index.js
+++ src/views/components/footer/index.js
@@ -8,11 +8,15 @@ export default Epoxy.View.extend({
   className: 'footer',
   events: {
     'click [data-js-href]': 'openSocial',
+    'click [data-js-epam-logo]': 'onClickEPAM',
   },
   initialize() {
     this.renderTemplate();
   },
   openSocial(e) {
     window.open($(e.currentTarget).attr('data-js-href'));
+  },
+  onClickEPAM() {
+    window.open('https://www.epam.com/');
   },
 });
