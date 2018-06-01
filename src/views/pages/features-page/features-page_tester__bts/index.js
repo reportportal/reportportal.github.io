8ee571798d2e0e5c8e2b0d74ccffbec6c5fce7fb
Yana_Zaitsava <Zaitsava_Yana@epam.com>
2018-06-01 14:55:31 +0300

    Bts animation fixes

--- src/views/pages/features-page/features-page_tester__bts/index.js
+++ src/views/pages/features-page/features-page_tester__bts/index.js
@@ -1,4 +1,5 @@
 import IndexPageSection from '../../sectionView';
+import { $ } from 'backbone';
 import template from './features-page_tester__bts.jade';
 import './features-page_tester__bts.scss';
 import './animation.scss';
@@ -7,7 +8,19 @@ export default IndexPageSection.extend({
   template,
   className: 'features-page_tester__bts',
   initialize() {
+    this.animationClass;
+    let elClass;
+    const isIE = false || !!document.documentMode;
+    const isEdge = !isIE && !!window.StyleMedia;
+    if (isIE || isEdge) {
+      elClass = 'ie-animation';
+      this.animationClass = 'ie-animate-bts';
+    } else {
+      elClass = 'default-animation';
+      this.animationClass = 'animate-bts';
+    }
     this.renderTemplate();
+    $(this.el).addClass(elClass);
   },
   checkScroll(scrollTop, scrollElTop) {
     if (scrollElTop > 250) {
@@ -20,7 +33,7 @@ export default IndexPageSection.extend({
     return [
       { checkScroll: this.checkScroll.bind(this),
         el: this.el,
-        controlAnimation: this.controlAnimation.bind(this, 'animate-bts'),
+        controlAnimation: this.controlAnimation.bind(this, this.animationClass),
       },
     ];
   },
