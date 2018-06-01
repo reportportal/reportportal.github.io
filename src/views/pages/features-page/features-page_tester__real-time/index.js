11bd73dd225450da6062c383d482222edab18bce
Yana_Zaitsava <Zaitsava_Yana@epam.com>
2018-06-01 13:43:47 +0300

    Real time animation fixes

--- src/views/pages/features-page/features-page_tester__real-time/index.js
+++ src/views/pages/features-page/features-page_tester__real-time/index.js
@@ -1,4 +1,5 @@
 import IndexPageSection from '../../sectionView';
+import { $ } from 'backbone';
 import template from './features-page_tester__real-time.jade';
 import './features-page_tester__real-time.scss';
 import './animation.scss';
@@ -7,7 +8,19 @@ export default IndexPageSection.extend({
   template,
   className: 'features-page_tester__real-time bg-darken',
   initialize() {
+    this.animationClass;
+    let elClass;
+    const isIE = false || !!document.documentMode;
+    const isEdge = !isIE && !!window.StyleMedia;
+    if (isIE || isEdge) {
+      elClass = 'ie-animation';
+      this.animationClass = 'ie-animate-realtime';
+    } else {
+      elClass = 'default-animation';
+      this.animationClass = 'animate-realtime';
+    }
     this.renderTemplate();
+    $(this.el).addClass(elClass);
   },
   checkScroll(scrollTop, scrollElTop) {
     if (scrollElTop > 200) {
@@ -20,7 +33,7 @@ export default IndexPageSection.extend({
     return [
       { checkScroll: this.checkScroll.bind(this),
         el: this.el,
-        controlAnimation: this.controlAnimation.bind(this, 'animate-real-time'),
+        controlAnimation: this.controlAnimation.bind(this, this.animationClass),
       },
     ];
   },
