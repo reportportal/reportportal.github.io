ee56e07b1b168f5dc350272172669177b3ab2f21
Mikhail Sidarkevich <mikhail_sidarkevich@epam.com>
2022-03-29 11:11:09 +0300

    EPMRPP-75800 || move reactWrapper to utils, move modal provider to top lvl

--- src/react-components/pricing-page/pricingPage.jsx
+++ src/react-components/pricing-page/pricingPage.jsx
@@ -15,18 +15,23 @@
  */
 
 import React from 'react';
-import reactWrapper from 'pages/pricing-page/reactWrapper';
+import reactWrapper from 'utils/reactWrapper';
 import BodyLayout from 'react-components/layouts/bodyLayout.jsx';
 import QuestionsForm from 'react-components/forms/questions-form/questionsForm.jsx';
 import PlansBlock from 'react-components/plans-block/plansBlock.jsx';
 import './pricingPage.scss';
 
-const Layout = () => <BodyLayout className="pricing-page">
-  <>
-    <div className="pricing-page-header">Flexible options for cloud<br /> or self hosted installation</div>
-    <PlansBlock />
-    <QuestionsForm />
-  </>
-</BodyLayout>;
+const Layout = () => (
+  <BodyLayout className="pricing-page">
+    <>
+      <div className="pricing-page-header">
+        Flexible options for cloud
+        <br /> or self hosted installation
+      </div>
+      <PlansBlock />
+      <QuestionsForm />
+    </>
+  </BodyLayout>
+);
 
 export default reactWrapper(Layout);
