0d03c5be707a5b734c2e85e91b5e645080c85aac
Vadzim Hvazdovich <vadzim_hvazdovich@epam.com>
2022-07-29 16:06:19 +0400

    EPMRPP-78645 || "Terms and Conditions" link leads to the wrong page

--- src/react-components/plans-block/plansBlock.jsx
+++ src/react-components/plans-block/plansBlock.jsx
@@ -175,7 +175,7 @@ const PlansBlock = () => {
         <div className={cx('pseudo-table')}>
           {plans}
           <div className={cx('note')}>Payment is made quarterly</div>
-          <a className={cx('terms')} target="_blank" href='https://reportportal.io/docs/Terms-&-Conditions' rel='noreferrer'>
+          <a className={cx('terms')} target="_blank" href='https://reportportal.io/legal/terms' rel='noreferrer'>
             Terms & Conditions
           </a>
         </div>
