086b1558f6d17878eb72631d1fd52b1129d17a86
Vadzim Hvazdovich <vadzim_hvazdovich@epam.com>
2022-08-08 19:45:04 +0400

    EPMRPP-78533 || code review fixes - 1

--- src/react-components/pages/blog-page/blogPage.jsx
+++ src/react-components/pages/blog-page/blogPage.jsx
@@ -33,18 +33,35 @@ const tags = [
   'datastreams',
 ];
 
+const elasticSearchBenefits  = [
+  'Reduction of database load used by pattern analysis up to 5x times;',
+  'Reduction of costs and time for maintaining the PostgreSQL database by decreasing its shape by at least 2 times;',
+  'Full text search capabilities for text logs is faster than in PostgreSQL for production-like data (x33 for text queries, less CPU utilization 1 – 16x times in comparison with PostgreSQL);',
+  'Less space for Log table in Elastic (x8.5 less);',
+  'Storage of PostgreSQL will be reduced by the 2nd largest table “Log” size (text message column + index for the column will be deleted);',
+  'Similar performance with PostgreSQL on getting logs by id;',
+  'Storing logs in different indices per project (and then per launch) allows to get per project data faster and reduces the risks of locks occurrence.',
+];
+
+const dataStreamsBenefits = [
+  'Logs deletion by IDs is x29 times faster in data streams compared to Indices;',
+  'Pretty fast logs reporting during high workload;',
+  'Creation of cheap data nodes for old data, e.g., HDD with low resources. ElasticSearch allows to configure the old data storage using ILM (Index Lifetime Management) policy. It might be useful, for example, if your project uses some information once per week/month, etc;',
+  'Various index rollover conditions – fast creation of the new generation. It means that a new generation of this data stream is created when the limit is reached (by logs count, by logs amount, by date). So, logs of this data stream proceed to the new generation. Limits can be specified in the IML policy.',
+];
+
 const BlogPage = () => (
   <BodyLayout className={cx('blog-page')}>
     <div className={cx('additional-background')} />
     <div className={cx('background')}>
       <div className={cx('background-content')}>
         <div className={cx('date')}>
-          July 24, 2022
+          August 8, 2022
         </div>
         <div className={cx('tags')}>
           {tags.map((tag, i) => (
             <span key={`tag${i}`} className={cx('tag')}>
-              {`#${tag}`}
+              #{tag}
             </span>
           ))}
         </div>
@@ -54,7 +71,6 @@ const BlogPage = () => (
       <div className={cx('title')}>
         ElasticSearch and Data Streams
       </div>
-
       <p>
         Storing and managing test logs inside ElasticSearch (via Data Streams) with double logging to PostgreSQL in parallel was implemented in version 5.7.2.
       </p>
@@ -62,66 +78,53 @@ const BlogPage = () => (
         Saving logs to ElasticSearch still can be turned off after installing 5.7.2, if needed. In this case log messages will be stored in PostgreSQL only. Double logging is activated to ElasticSearch and to PostgreSQL, if there are configured settings for ElasticSearch in API and Jobs services. Please, note that settings must be added or absent for both services. A full migration of logs to ElasticSearch is planned in version 5.8. Before that, the functionality will be implemented step by step.
       </p>
       <img src={appImg} alt="report portal app" />
-      <div className={cx('inner-header')}>
+      <h3>
         Why we use ElasticSearch?
-      </div>
+      </h3>
       <p>
         PostgreSQL was previously used as a database for log storage, but – according to the performance tests – this is not the most effective way. Log messages take up the most space in the database, so we decided to transfer them to ElasticSearch. Logs migration to ElasticSearch will significantly reduce storage occupied by the log table. It will improve overall database performance (time, costs).
       </p>
       <img src={elasticSearchImg} alt="elasticSearch" />
-      <div className={cx('inner-header')}>
+      <h3>
         ElasticSearch benefits (after a full migration)
-      </div>
-      <div className={cx('list')}>
-        {[
-          'Reduction of database load used by pattern analysis up to 5x times;',
-          'Reduction of costs and time for maintaining the PostgreSQL database by decreasing its shape by at least 2 times;',
-          'Full text search capabilities for text logs is faster than in PostgreSQL for production-like data (x33 for text queries, less CPU utilization 1 – 16x times in comparison with PostgreSQL);',
-          'Less space for Log table in Elastic (x8.5 less);',
-          'Storage of PostgreSQL will be reduced by the 2nd largest table “Log” size (text message column + index for the column will be deleted);',
-          'Similar performance with PostgreSQL on getting logs by id;',
-          'Storing logs in different indices per project (and then per launch) allows to get per project data faster and reduces the risks of locks occurrence.',
-          ].map((item, i) => (
-            <div key={`list-item${i}`} className={cx('list-item')}>
+      </h3>
+      <ul className={cx('list')}>
+        {elasticSearchBenefits.map((item, i) => (
+            <li key={`list-item${i}`} className={cx('list-item')}>
               <div className={cx('list-item-text')}>
                 {item}
               </div>
-            </div>
+            </li>
         ))}
-      </div>
+      </ul>
       <img src={storageImg} alt="rows of logs and storage usage" />
       <div className={cx('notice')}>
         <div className={cx('notice-text')}>
           ElasticSearch is lower by storage up to <span className={cx('blue', 'big')}>8.5x</span> times.
         </div>
       </div>
-      <div className={cx('inner-header')}>
+      <h3>
         Why we use Data Streams?
-      </div>
+      </h3>
       <p>
         Elasticsearch provides a special approach for storing log data: “A data stream lets you store append-only time series data across multiple indices while giving you a single named resource for requests. Data Streams are well-suited for logs, events, metrics, and other continuously generated data,” – described in the official elastic search documentation.
       </p>
-      <div className={cx('inner-header', 'with-margin-top')}>
+      <h3 className={cx('with-margin-top')}>
         Data Streams benefits
-      </div>
-      <div className={cx('list')}>
-        {[
-          'Logs deletion by IDs is x29 times faster in data streams compared to Indices;',
-          'Pretty fast logs reporting during high workload;',
-          'Creation of cheap data nodes for old data, e.g., HDD with low resources. ElasticSearch allows to configure the old data storage using ILM (Index Lifetime Management) policy. It might be useful, for example, if your project uses some information once per week/month, etc;',
-          'Various index rollover conditions – fast creation of the new generation. It means that a new generation of this data stream is created when the limit is reached (by logs count, by logs amount, by date). So, logs of this data stream proceed to the new generation. Limits can be specified in the IML policy.',
-        ].map((item, i) => (
-          <div key={`list-item${i}`} className={cx('list-item')}>
+      </h3>
+      <ul className={cx('list')}>
+        {dataStreamsBenefits.map((item, i) => (
+          <li key={`list-item${i}`} className={cx('list-item')}>
             <div className={cx('list-item-text')}>
               {item}
             </div>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
       <img src={responseTimesImg} alt="response times over time" />
-      <div className={cx('inner-second-header')}>
+      <h4>
         Response times table (95pct)
-      </div>
+      </h4>
       <table className={cx('table')}>
         <tr>
           <th>Source</th>
@@ -151,9 +154,9 @@ const BlogPage = () => (
       <div className={cx('simple-notice')}>
         You can find more details about ElasticSearch Data Streams <a target="_blank" href='https://opster.com/guides/elasticsearch/data-architecture/elasticsearch-data-streams/' rel="noreferrer">here.</a>
       </div>
-      <div className={cx('inner-header')}>
+      <h3>
         What effort is required from users?
-      </div>
+      </h3>
       <p>
         We recommend updating to version 5.7.2 for smooth transition of full logging to ElasticSearch, especially if you have many logs. If you update to version 5.7.2, use it for 3-4 months before version 5.8. This period will be enough for the vast of projects to generate enough logs history inside ElasticSearch. And then update to version 5.8 once it available. Since all logs will already be stored in ElasticSearch, no efforts will be required to do the migration. Along with version 5.8 we will distribute a migration script and instructions for data migration. So that you can easily migrate from early 5.x version.
       </p>
