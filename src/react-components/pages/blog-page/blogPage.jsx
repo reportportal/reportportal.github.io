aef0d915f388d3eb62168e3e03c24131cc4d7329
Vadim73i <55870906+Vadim73i@users.noreply.github.com>
2022-09-05 21:15:23 +0400

    Release: update blog article and 5gb docker warning add (#250)

--- src/react-components/pages/blog-page/blogPage.jsx
+++ src/react-components/pages/blog-page/blogPage.jsx
@@ -33,25 +33,30 @@ const tags = [
   'datastreams',
 ];
 
-const elasticSearchBenefits  = [
-  'Reduction of database load used by pattern analysis up to 5x times;',
-  'Reduction of costs and time for maintaining the PostgreSQL database by decreasing its shape by at least 2 times;',
-  'Full text search capabilities for text logs is faster than in PostgreSQL for production-like data (x33 for text queries, less CPU utilization 1 – 16x times in comparison with PostgreSQL);',
-  'Less space for Log table in Elastic (x8.5 less);',
-  'Storage of PostgreSQL will be reduced by the 2nd largest table “Log” size (text message column + index for the column will be deleted);',
-  'Similar performance with PostgreSQL on getting logs by id;',
-  'Storing logs in different indices per project (and then per launch) allows to get per project data faster and reduces the risks of locks occurrence.',
+const transition = [
+  'ReportPortal v5.7 is the last version with log storage in PostgreSQL.',
+  'ReportPortal v5.7.2 is the version with a double logging mechanism: logs are stored both in PG and Elastic at the moment of entry.',
+  'ReportPortal v5.8 is the version with logs storage in Elastic only.',
+];
+
+const elasticSearchBenefits = [
+  <>Reduced disk space usage, with a smaller footprint <span className={cx('semibold')}>up to x8.5.</span></>,
+  <>Reduced maintenance of the PostgreSQL database, and reduced requirements for the shape sizes by at <span className={cx('semibold')}>least x2 times.</span></>,
+  <>Reduction of database load used by pattern analysis up to <span className={cx('semibold')}>5x times.</span></>,
+  'Full-text search capabilities for text logs (x33 times quicker for text queries, and less CPU utilization 1 – 16x times in comparison with PostgreSQL).',
+  'Similar performance with PostgreSQL on getting logs by ID.',
+  'Storing logs in different indices per project allows to get project data faster and reduces the risks of locks occurrence.',
 ];
 
 const dataStreamsBenefits = [
-  'Logs deletion by IDs is x29 times faster in data streams compared to Indices;',
-  'Pretty fast logs reporting during high workload;',
-  'Creation of cheap data nodes for old data, e.g., HDD with low resources. ElasticSearch allows to configure the old data storage using ILM (Index Lifetime Management) policy. It might be useful, for example, if your project uses some information once per week/month, etc;',
-  'Various index rollover conditions – fast creation of the new generation. It means that a new generation of this data stream is created when the limit is reached (by logs count, by logs amount, by date). So, logs of this data stream proceed to the new generation. Limits can be specified in the IML policy.',
+  <>Logs deletion by IDs is <b>x29 times faster</b> in data streams compared to Indices.</>,
+  'Fast logs insertion (reporting) at the time of the high workload.',
+  'Creation of cheap data nodes for old data, e.g., HDD with low resources. ElasticSearch allows configuring the old data storage using ILM (Index Lifetime Management) policy. It might be useful, for example, if your project uses some information once per week/month, etc.',
+  'Various index rollover conditions – fast creation of the new generation. It means that a new generation of this data stream is created when the limit is reached (by logs count, by logs amount, by date). So, logs of this data stream proceed to the new generation. Limits can be specified in the IML policy per project.',
 ];
 
 const notes = [
-  'Before version 5.8, double logging might increase the resources – processor and dick space.',
+  'Before version 5.8, double logging might increase the resources usage – CPU, disk space.',
   'We are already using the ElasticSearch license, so, no new license is required. For now, we stay on version 7.10 with Apache 2.0. We might switch to OpenSearch in prospect.',
 ];
 
@@ -61,7 +66,7 @@ const BlogPage = () => (
     <div className={cx('background')}>
       <div className={cx('background-content')}>
         <div className={cx('date')}>
-          August 8, 2022
+          September 5, 2022
         </div>
         <div className={cx('tags')}>
           {tags.map((tag, i) => (
@@ -74,32 +79,83 @@ const BlogPage = () => (
     </div>
     <div className={cx('content')}>
       <div className={cx('title')}>
-        ElasticSearch and Data Streams
+        ReportPortal moves test logs from PostgreSQL to Elastic-type engines
       </div>
       <p>
-        Storing and managing test logs inside ElasticSearch (via Data Streams) with double logging to PostgreSQL in parallel was implemented in version 5.7.2.
+        The ReportPortal’s essence is based on assistance in working with automated testing results, and it all starts with the aggregation of results at a single place. For a long time, the relational database served well as a storage place for us. But the more test cases you run, the more storage you need to keep all related logs.
       </p>
       <p>
-        Saving logs to ElasticSearch still can be turned off after installing 5.7.2, if needed. In this case log messages will be stored in PostgreSQL only. Double logging is activated to ElasticSearch and to PostgreSQL, if there are configured settings for ElasticSearch in API and Jobs services. Please, note that settings must be added or absent for both services. A full migration of logs to ElasticSearch is planned in version 5.8. Before that, the functionality will be implemented step by step.
+        Before we introduced PostgreSQL with ReportPortal v5.0, we were using MongoDB from version 1 to version 4. MongoDB is a document-oriented non-relational database, was pretty good at writing the logs, but was a kind of nightmare to track metrics, build charts and find insights across testing results.
+      </p>
+      <p>
+        PostgreSQL resolved data joins for us and made implementing new features much easier and quicker. But it also brought some drawbacks: such as excessive disk space usage to keep and rotate logs, slowdowns on data inserting, high CPU loads on data deletion and constant required maintenance (VACUUM clean).
+      </p>
+      <p>
+        There are several reasons why <a target="_blank" href="https://dba.stackexchange.com/questions/123627/postgresql-data-files-have-size-more-than-data-itself" rel="noreferrer">PostgreSQL uses more disk space than the data itself</a>, and it comes mainly from indexes of the data and several copies of the same data, due to specifics of PostgreSQL mechanics.
+      </p>
+      <p>
+        Logs are a big portion of data inside ReportPortal. And assuming specifics of the workflow associated with it, it should be constantly added and deleted, without much of the need to keep it for a long time. Even ReportPortal’s Machine Learning in Auto-Analysis feature will use it just several times for training and will store it transformed inside the ML model. But constant insertions and deletions will produce a significant load and innumerous storage footprint.
+      </p>
+      <p>
+        By reconsidering all pros and cons, we made a deep investigation and performance analysis of ElasticSearch vs PostgreSQL from the perspective of ReportPortal’s load model and nature of data. The results of this investigation go below in this article.
+      </p>
+      <p>
+        Assuming these results, our architectural decision was to switch logs storage from PostgreSQL to ElasticSearch, well… because we already have it as a part of our application. And since it gives us such benefits as <b>reduced storage footprint up to 8.5x times</b>, and <b>data deletion up to 29x times quicker</b> with just a fraction of the CPU load.
+      </p>
+      <p>
+        And also, it opens up the capabilities of the full-text search!
+      </p>
+      <p>
+        Please read our findings below.
+      </p>
+      <h3 className={cx('with-margin-top')}>
+        Smooth transition of logs from PostgreSQL to ElasticSearch Data Streams
+      </h3>
+      <p>
+        To minimize the effort of our users with data migration, we decided to introduce the interim version (5.7.2) with double-logging. Here is how it looks in the version:
+      </p>
+      <ul className={cx('list')}>
+        {transition.map((item, i) => (
+          <li key={`list-item${i}`} className={cx('list-item')}>
+            <div className={cx('list-item-text')}>
+              {item}
+            </div>
+          </li>
+        ))}
+      </ul>
+      <p>
+        The average time frame of 3 months between releases will give time for Elastic to pre-aggregate enough data for a smooth daily routine. The upgrade to the version 5.8 will turn off the logs in PostgreSQL and clean it up. Along with the v5.8 we will still release the migration scripts, which will do most of the work automatically. But in order to minimize downtime, we recommend you use 5.7.2 for a while.
+      </p>
+      <p>
+        There is still an option to turn off saving of the logs into ElasticSearch in v5.7.2, if needed. In this case log messages will be stored in PostgreSQL only. Double logging is active if there are configured settings for ElasticSearch in both `service-api` and `service-jobs` services. Please, note that configurations must be added or absent for both services. Nevertheless, a full migration of logs to ElasticSearch is planned in version 5.8.
+      </p>
+      <p>
+        Here is some visual representation for reference.
       </p>
       <img src={appImg} alt="report portal app" />
       <h3>
-        Why we use ElasticSearch?
+        Why ElasticSearch?
       </h3>
       <p>
-        PostgreSQL was previously used as a database for log storage, but – according to the performance tests – this is not the most effective way. Log messages take up the most space in the database, so we decided to transfer them to ElasticSearch. Logs migration to ElasticSearch will significantly reduce storage occupied by the log table. It will improve overall database performance (time, costs).
+        PostgreSQL was previously used as a database for log storage, but – according to the performance tests – this is not the most effective way. Log messages take up the most space in the database, so we decided to transfer them to ElasticSearch. Logs migration to ElasticSearch will significantly reduce storage occupied by the log table. It will improve overall database performance (timings and costs of infrastructure).
+      </p>
+      <p>
+        In version 5.8 ReportPortal will still rely on ElasticSearch v7.10, as the last available version under Apache 2.0 license. And going forward we will consider Elastic-like solutions and forks, but this will come into play after a series of performance analyses and investigations. So please stay tuned, we know that it could be an issue for some of our users, but in order to avoid hasty decisions we have to make it incrementally.
       </p>
       <img src={elasticSearchImg} alt="elasticSearch" />
       <h3>
-        ElasticSearch benefits (after a full migration)
+        What would you get with the ReportPortal v5.8 and the switch to the ElasticSearch logging?
       </h3>
-      <ul className={cx('list')}>
+      <p>
+        It’s not something that you will see at the first moment, but definitely will benefit you in the long run, with outcomes like:
+      </p>
+      <ul className={cx('simple-list')}>
         {elasticSearchBenefits.map((item, i) => (
-            <li key={`list-item${i}`} className={cx('list-item')}>
-              <div className={cx('list-item-text')}>
-                {item}
-              </div>
-            </li>
+          <li key={`simple-list-item${i}`} className={cx('simple-list-item')}>
+            <div className={cx('simple-list-item-text')}>
+              {item}
+            </div>
+          </li>
         ))}
       </ul>
       <img src={storageImg} alt="rows of logs and storage usage" />
@@ -112,15 +168,15 @@ const BlogPage = () => (
         Why we use Data Streams?
       </h3>
       <p>
-        Elasticsearch provides a special approach for storing log data: “A data stream lets you store append-only time series data across multiple indices while giving you a single named resource for requests. Data Streams are well-suited for logs, events, metrics, and other continuously generated data,” – described in the official elastic search documentation.
+        Elasticsearch provides a special approach for storing log data: <span className={cx('italic')}>“A data stream lets you store append-only time series data across multiple indices while giving you a single named resource for requests. Data Streams are well-suited for logs, events, metrics, and other continuously generated data,”</span> – described in the <a target="_blank" href="https://www.elastic.co/guide/en/elasticsearch/reference/current/data-streams.html#data-streams" rel="noreferrer">official elastic search documentation.</a>
       </p>
       <h3 className={cx('with-margin-top')}>
         Data Streams benefits
       </h3>
-      <ul className={cx('list')}>
+      <ul className={cx('simple-list')}>
         {dataStreamsBenefits.map((item, i) => (
-          <li key={`list-item${i}`} className={cx('list-item')}>
-            <div className={cx('list-item-text')}>
+          <li key={`simple-list-item${i}`} className={cx('simple-list-item')}>
+            <div className={cx('simple-list-item-text')}>
               {item}
             </div>
           </li>
@@ -163,7 +219,7 @@ const BlogPage = () => (
         What effort is required from users?
       </h3>
       <p>
-        We recommend updating to version 5.7.2 for smooth transition of full logging to ElasticSearch, especially if you have many logs. If you update to version 5.7.2, use it for 3-4 months before version 5.8. This period will be enough for the vast of projects to generate enough logs history inside ElasticSearch. And then update to version 5.8 once it available. Since all logs will already be stored in ElasticSearch, no efforts will be required to do the migration. Along with version 5.8 we will distribute a migration script and instructions for data migration. So that you can easily migrate from early 5.x version.
+        We recommend updating to version 5.7.2 for a smooth transition of full logging to ElasticSearch, especially if you have many logs. If you update to version 5.7.2, use it for 3-4 months before version 5.8. This period will be enough for the vast majority of projects to generate enough logs history inside ElasticSearch. And then update to version 5.8 once it is available. Since all logs will already be stored in ElasticSearch, no efforts will be required to do the migration. Along with version 5.8 we will distribute a migration script and instructions for data migration so that you can easily migrate from the early 5.x version.
       </p>
       {notes.map((note, i) => (
         <div key={`note${i}`} className={cx('note')}>
@@ -174,7 +230,7 @@ const BlogPage = () => (
         </div>
       ))}
       <p>
-        To summarize, using of ElasticSearch and Data Streams will bring significant performance benefits in the future.
+        To summarize, using of ElasticSearch and Data Streams will bring significant performance benefits.
       </p>
     </div>
   </BodyLayout>
