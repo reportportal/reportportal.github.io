f68b18570daf4c843b0bc5329517807a7da797eb
Viktor Soroka <viktor_soroka@epam.com>
2023-01-20 14:38:29 +0200

    Setup deploy-dev

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,6 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: process.env.CONTENTFUL_HOST,
       },
     },
   ],
