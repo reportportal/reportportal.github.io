77267513c6c06c56db60ba5e694840622fa510aa
Viktor Soroka <viktor_soroka@epam.com>
2023-02-25 18:35:05 +0200

    Use CSS grid for subscription form

--- src/components/SubscriptionForm/SubscriptionForm.jsx
+++ src/components/SubscriptionForm/SubscriptionForm.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import cx from 'classnames';
 
 import { createBemBlockBuilder } from '../../utils';
 
 import './SubscriptionForm.scss';
-import cx from 'classnames';
 
 export const SubscriptionForm = ({ isSubmitted, isAlreadySubscribed }) => {
   const { register, handleSubmit, formState } = useForm();
   const getBlocksWith = createBemBlockBuilder(['subscription-form']);
+
+  const isFormInvalid = formState.isSubmitted && !formState.isValid;
   const onSubmit = () => {};
 
   if (isSubmitted) {
@@ -34,27 +36,26 @@ export const SubscriptionForm = ({ isSubmitted, isAlreadySubscribed }) => {
   }
 
   return (
-    <form className={getBlocksWith('__form')} onSubmit={handleSubmit(onSubmit)}>
+    <form
+      className={cx(getBlocksWith('__form'), getBlocksWith('__form--error'))}
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className={getBlocksWith('__form-group')}>
         <input
           className={getBlocksWith('__form-input')}
           {...register('email', { required: true, pattern: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g })}
         />
-        {formState.isSubmitted && formState.errors.email?.type === 'pattern' && (
-          <span className={getBlocksWith('__form-error')}>Please use a valid email format</span>
-        )}
-        <span className={getBlocksWith('__form-info')}>
-          By subscribing, you agree to receive marketing emails from Report Portal team and
-          associated partners and accept our <a href="https://contentful.com/">Privacy Policy</a>
-        </span>
       </div>
-      <button
-        className={cx('btn', 'btn--primary')}
-        disabled={formState.isSubmitted && !formState.isValid}
-        type="submit"
-      >
+      <button className={cx('btn', 'btn--primary')} disabled={isFormInvalid} type="submit">
         Subscribe
       </button>
+      {formState.isSubmitted && formState.errors.email?.type === 'pattern' && (
+        <span className={getBlocksWith('__form-error')}>Please use a valid email format</span>
+      )}
+      <span className={getBlocksWith('__form-info')}>
+        By subscribing, you agree to receive marketing emails from Report Portal team and associated
+        partners and accept our <a href="https://contentful.com/">Privacy Policy</a>
+      </span>
     </form>
   );
 };
