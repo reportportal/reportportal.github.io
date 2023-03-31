53c2b7f9a045faaa7664b6420daab8af95b58f59
Viktor Soroka <viktor_soroka@epam.com>
2023-03-31 17:45:16 +0300

    Hero block. 'Why Report Portal' section (#317)
    * Implement hero block
    
    * Code cleanup
    
    * Implement `Why Report Portal` section
    
    * Implement sticky header
    
    * Design review adjustments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,16 @@
 import React from 'react';
-import { graphql } from 'gatsby';
-import get from 'lodash/get';
 
 import { Layout } from '../components/Layout';
-import { Hero } from '../components/Hero';
-import { ArticlePreview } from '../components/ArticlePreview';
+import { Showcase } from '../components/Showcase';
+import { WhyReportPortal } from '../components/WhyReportPortal';
 
-class RootIndex extends React.Component {
-  render() {
-    const posts = get(this, 'props.data.allContentfulBlogPost.nodes');
-    const [author] = get(this, 'props.data.allContentfulPerson.nodes');
-
-    return (
-      <Layout location={this.props.location}>
-        <Hero
-          image={author.heroImage?.gatsbyImage}
-          title={author?.name}
-          content={author?.shortBio}
-        />
-        <ArticlePreview posts={posts} />
-      </Layout>
-    );
-  }
-}
+const RootIndex = ({ location }) => {
+  return (
+    <Layout location={location}>
+      <Showcase />
+      <WhyReportPortal />
+    </Layout>
+  );
+};
 
 export default RootIndex;
-
-export const pageQuery = graphql`
-  query HomeQuery {
-    allContentfulBlogPost(sort: { date: DESC }) {
-      nodes {
-        date(formatString: "MMMM Do, YYYY")
-        title {
-          title
-        }
-        author
-        id
-      }
-    }
-    allContentfulPerson(filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }) {
-      nodes {
-        name
-        shortBio {
-          raw
-        }
-        heroImage: image {
-          gatsbyImage(layout: CONSTRAINED, placeholder: BLURRED, width: 1180)
-        }
-      }
-    }
-  }
-`;
