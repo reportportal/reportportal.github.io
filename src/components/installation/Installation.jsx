48eb51a289a9c02df588240cdd9622686680be89
Vasyl Kurtash <vasyl_kurtash@epam.com>
2023-04-20 09:38:51 +0300

    review fixes

--- src/components/installation/Installation.jsx
+++ src/components/installation/Installation.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useBoolean } from 'ahooks';
-import cx from 'classnames';
 
 import { Container } from '../Container';
 import { createBemBlockBuilder } from '../../utils';
@@ -18,26 +17,12 @@ const buttons = [
   {
     text: 'With Docker',
     active: true,
-    iconComponent: (item) => (
-      <DockerIcon
-        className={cx({
-          'installation__active-icon': item.active,
-          installation__icon: !item.active,
-        })}
-      />
-    ),
+    iconComponent: () => <DockerIcon />,
   },
   {
     text: 'With Kubernetes',
     active: false,
-    iconComponent: (item) => (
-      <KubernetesIcon
-        className={cx({
-          'installation__active-icon': item.active,
-          installation__icon: !item.active,
-        })}
-      />
-    ),
+    iconComponent: () => <KubernetesIcon />,
   },
 ];
 
