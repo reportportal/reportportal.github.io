406b58db18b6cfd4f78d1dd5e437455472024250
Vadim73i <55870906+Vadim73i@users.noreply.github.com>
2023-04-26 14:02:24 +0400

    Blog post text update (#339)

--- src/react-components/pages/blog/versions-blog-page/versionsBlogPage.jsx
+++ src/react-components/pages/blog/versions-blog-page/versionsBlogPage.jsx
@@ -20,7 +20,7 @@ import BlogPageHeader from '../common/blog-page-header/blogPageHeader';
 import BlogPageContent from '../common/blog-page-content/blogPageContent';
 import List from '../common/list/list';
 
-const releases = ['Semi annual product releases', 'Regular service release'];
+const releases = ['Semi annual product releases', 'Regular service releases'];
 
 const VersionsBlogPage = () => (
   <>
@@ -29,9 +29,9 @@ const VersionsBlogPage = () => (
       date={'April 28, 2023'}
       background={VersionsBackgroundImage}
     />
-    <BlogPageContent title={'New approach to versions naming'}>
+    <BlogPageContent title={'The new approach to versions naming'}>
       <p>
-        Our team aims to release often and provide more flexibility for our users and ReportPortal team itself in feature implementation and delivery. Our priority is always to deliver the product to our users as soon as we can (without sacrificing the product quality). That gives us an opportunity to collect users' feedback earlier and adjust our development strategy following your needs and market trends.
+        Our team aims to release often and provide more flexibility for our users and the ReportPortal team itself in feature implementation and delivery. Our priority is always to deliver the product to our users as soon as we can (without sacrificing the product quality). That gives us an opportunity to collect users' feedback earlier and adjust our development strategy following your needs and market trends.
       </p>
       <p>
         Based on the above, we decided to review our versioning models and release flow and move toward the new ones:
@@ -43,16 +43,16 @@ const VersionsBlogPage = () => (
         The previous approach to versioning
       </h3>
       <p>
-        Previously we used the approach to versions when the services were updated all together. For example, if there were changes in the API service, Auth service and UI service that are not related to each other, we still made a release of these services trying to link them with a common version. Using this approach, we tried to understand in advance in which service and ReportPortal version the next changes will occur. We always synchronized all our components by the biggest change.
+        Previously we used the approach to versions when the services were updated together. For example, if there were changes in the API service, Auth service and UI service that are not related to each other, we still made a release of these services trying to link them with a common version. Using this approach, we tried to understand in advance in which service and ReportPortal version the next changes will occur. We always synchronized all our components by the biggest change.
       </p>
       <p>
-        With this approach, we often had to reschedule some features release when they were almost ready, but suddenly we urgently needed to update some component. Synchronization of services was causing problems, was painful and hard for the development process.
+        With this approach, we often had to reschedule some features' releases when they were almost ready, but suddenly we urgently needed to update some components. Synchronization of services was causing problems and was hard for the development process.
       </p>
       <p>
-        So, we decided to change the approach to versions. Now we have replaced the Product release approach into separate Product release and Service release.
+        So, we decided to change the approach to versions. Now we have replaced the Product release approach with separate Product release and Service release.
       </p>
       <h3>
-        The main difference of a new approach
+        The main difference of the new approach
       </h3>
       <p>
         With the new approach, all our components (API Service, Auth Service, UI Service, Analyzer Service, Job Service) will have their own independent versions. We refused to synchronize versions. For example, if the API Service has major changes at the release time, and the UI Service and Analyzer Service have minor or patch changes, then their versions will be different. In addition, if there is no connection between their changes, we can release them separately upon readiness.
@@ -76,7 +76,7 @@ const VersionsBlogPage = () => (
         We use <a target="_blank" href='https://semver.org/' rel="noreferrer">Semantic versioning</a> for Service release.
       </p>
       <p>
-        New approach to versions allows us to make fast implementation, testing, and delivery of new features to our users as well as collect and incorporate users feedback in our product. Besides functional changes this approach will allow us to release non-functional changes like security fixes in base image or dependencies more easily.
+        The new approach to versions allows us to make fast implementation, testing, and delivery of new features to our users as well as collect and incorporate users' feedback in our product. Besides functional changes this approach will allow us to release non-functional changes like security fixes in the base image or dependencies more easily.
       </p>
     </BlogPageContent>
   </>
