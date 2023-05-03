9b292db19075ede8e0a485c19b45ce71db3ea030
Viktor Soroka <viktor_soroka@epam.com>
2023-05-03 15:14:57 +0300

    Implement customers statistics section (#340)

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Layout } from '../components/Layout';
 import { Showcase } from '../components/Showcase';
 import { WhyReportPortal } from '../components/WhyReportPortal';
+import { CustomersStatistics } from '../components/CustomersStatistics';
 
 const RootIndex = ({ location }) => {
   return (
     <Layout location={location}>
       <Showcase />
       <WhyReportPortal />
+      <CustomersStatistics />
     </Layout>
   );
 };
