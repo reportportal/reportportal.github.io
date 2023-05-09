b7af5aa867d53d85571c7de141c17c98ab188454
Vasyl Kurtash <vasyl_kurtash@epam.com>
2023-05-09 12:21:15 +0300

    review fixes

--- src/components/ScrollIndicator/ScrollIndicator.jsx
+++ src/components/ScrollIndicator/ScrollIndicator.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-scroll';
+
 import { createBemBlockBuilder } from '../../utils';
 
 import '../InstallationPage/InstallationPage.scss';
@@ -6,38 +8,29 @@ import './ScrollIndicator.scss';
 
 const getBlocksWith = createBemBlockBuilder(['indicatory']);
 
-import { Link } from 'react-scroll';
-
-export const ScrollIndicator = ({ sections }) => {
-  const getSubstring = (title, substringNumber) => title.split('\n')[substringNumber];
-
-  return (
-    <>
-      {sections && (
-        <div className={getBlocksWith()}>
-          <div className={getBlocksWith('__box')}>
-            {sections.map((section) => (
-              <div key={section.id} className={getBlocksWith('__box-item')}>
-                <Link
-                  className={getBlocksWith('__link')}
-                  activeClass="active"
-                  to={section.id}
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                >
-                  <span>{getSubstring(section.title, 0)}</span>
-                  <br />
-                  <span className={getBlocksWith('__box-item-substring')}>
-                    {getSubstring(section.title, 1)}
-                  </span>
-                </Link>
-              </div>
-            ))}
-          </div>
+export const ScrollIndicator = ({ sections }) => (
+  <>
+    {sections && (
+      <div className={getBlocksWith()}>
+        <div className={getBlocksWith('__box')}>
+          {sections.map((section) => (
+            <div key={section.id} className={getBlocksWith('__box-item')}>
+              <Link
+                className={getBlocksWith('__link')}
+                activeClass="active"
+                to={section.id}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                {section.step && <span>{section.step}</span>}
+                <span className={getBlocksWith('__box-item-substring')}>{section.title}</span>
+              </Link>
+            </div>
+          ))}
         </div>
-      )}
-    </>
-  );
-};
+      </div>
+    )}
+  </>
+);
