22f3b84cc239b725d31597e85dcccb625547b045
KrDior <45118964+KrDior@users.noreply.github.com>
2023-07-02 20:27:40 +0300

    Implement features panel sticky and smooth scrolling (#357)
    * features panel sticky and smooth scrolling
    
    * fix scrolling position
    
    * fix review comments
    
    * fix review comments 2
    
    * fix import order
    
    * resolve MR comments 3
    
    * handle active menu on scroll
    
    * resolve comments 4
    
    * resolve comments 5
    
    * resolve comments 6
    
    * resolve comments 7
    
    ---------
    
    Co-authored-by: Saveli Savich <Saveli_Savich@epam.com>

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,6 @@
+export const tabletSm = 769;
+export const desktopSm = 1239;
+
+export const mediaTabletSm = `(min-width: ${tabletSm}px)`;
+
+export const mediaDesktopSm = `(min-width: ${desktopSm}px)`;
