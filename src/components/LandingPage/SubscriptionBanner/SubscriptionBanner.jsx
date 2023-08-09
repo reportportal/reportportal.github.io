ff5609c73b8e43a962828f0f2bce097bcaf41686
Viktor Soroka <viktor_soroka@epam.com>
2023-08-09 15:26:12 +0300

    Design review adjustments

--- src/components/LandingPage/SubscriptionBanner/SubscriptionBanner.jsx
+++ src/components/LandingPage/SubscriptionBanner/SubscriptionBanner.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { createBemBlockBuilder } from '../../../utils';
 import { SubscriptionForm } from '../../SubscriptionForm';
@@ -8,13 +8,23 @@ import './SubscriptionBanner.scss';
 
 const getBlocksWith = createBemBlockBuilder(['subscription-banner']);
 
-export const SubscriptionBanner = () => (
-  <div className={getBlocksWith()}>
-    <Banner
-      title="Stay in the know"
-      subtitle="Get the latest ReportPortal news, product updates and articles via email"
-    >
-      <SubscriptionForm />
-    </Banner>
-  </div>
-);
+export const SubscriptionBanner = () => {
+  const [subscriptionFormState, setSubscriptionFormState] = useState({
+    isSubmitted: false,
+    isAlreadySubscribed: false,
+  });
+
+  return (
+    <div className={getBlocksWith()}>
+      <Banner
+        title="Stay in the know"
+        subtitle="Get the latest ReportPortal news, product updates and articles via email"
+      >
+        <SubscriptionForm
+          subscriptionFormState={subscriptionFormState}
+          setSubscriptionFormState={setSubscriptionFormState}
+        />
+      </Banner>
+    </div>
+  );
+};
