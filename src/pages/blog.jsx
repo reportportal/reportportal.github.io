82aa235c82fb12529ae0e5c85b12252404f17862
Leandro Cunha <leandro_cunha@epam.com>
2023-08-17 23:55:51 +0200

    feat: blog page CSS improvements

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,39 +1,53 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import get from 'lodash/get';
+import React, { useState } from 'react'
+import { graphql } from 'gatsby'
+import cx from 'classnames'
 
-import { ArticlePreview } from '../components/ArticlePreview';
-import { Layout } from '../components/Layout';
-import { createBemBlockBuilder } from '../utils';
+import { ArticlePreview } from '../components/ArticlePreview'
+import { Layout } from '../components/Layout'
+import { createBemBlockBuilder } from '../utils'
 
-import '../components/BlogPage/BlogPage.scss';
+import '../components/BlogPage/BlogPage.scss'
 
-const getBlocksWith = createBemBlockBuilder(['blog']);
+const PAGE_SZIE = 9;
+const getBlocksWith = createBemBlockBuilder(['blog'])
 
-class BlogIndex extends React.Component {
-  render() {
-    const posts = get(this, 'props.data.allContentfulBlogPost.nodes');
+const BlogIndex = (props) => {
+  const {
+    data: {
+      allContentfulBlogPost: { nodes },
+    },
+    location,
+  } = props
 
-    console.log(posts);
-    return (
-      <Layout location={this.props.location}>
-        <div>
-          <div className={getBlocksWith()}>
-            <div className="container">
-              <h1 className={getBlocksWith('__title')}>Blog</h1>
-              <p className={getBlocksWith('__subtitle')}>
-                Product updates, news and technology articles
-              </p>
-              <ArticlePreview posts={posts} />
-            </div>
+  const [posts, setPosts] = useState(nodes.slice(0, PAGE_SZIE))
+
+  const handleClick = () => setPosts(nodes.slice(0, posts.length + PAGE_SZIE))
+
+  return (
+    <Layout location={location}>
+      <div>
+        <div className={getBlocksWith()}>
+          <div className='container'>
+            <h1 className={getBlocksWith('__title')}>Blog</h1>
+            <p className={getBlocksWith('__subtitle')}>
+              Product updates, news and technology articles
+            </p>
+            <ArticlePreview posts={posts} />
+            {posts.length < nodes.length && (
+              <div className={getBlocksWith('__footer')}>
+                <button className={cx('btn', 'btn--outline', 'btn--large')} onClick={handleClick}>
+                  Load more
+                </button>
+              </div>
+            )}
           </div>
         </div>
-      </Layout>
-    );
-  }
+      </div>
+    </Layout>
+  )
 }
 
-export default BlogIndex;
+export default BlogIndex
 
 export const pageQuery = graphql`
   query BlogIndexQuery {
@@ -58,4 +72,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
