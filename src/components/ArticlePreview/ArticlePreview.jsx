1c46e1066b18e5191ee988c6627c85fb27ca9032
Leandro Cunha <leandroscunha@gmail.com>
2023-08-18 17:12:14 +0200

    feat: test pre-commit hook

--- src/components/ArticlePreview/ArticlePreview.jsx
+++ src/components/ArticlePreview/ArticlePreview.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { renderRichText } from 'gatsby-source-contentful/rich-text';
+import * as styles from './ArticlePreview.scss';
 
 import { ArticleAuthor } from '../ArticleAuthor/ArticleAuthor';
-
-import * as styles from './ArticlePreview.scss';
 
 export const ArticlePreview = ({ posts }) => {
   if (!posts) return null;
