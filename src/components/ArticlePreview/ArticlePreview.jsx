4b16daaad98b076d280cb05120c4fbeca4566fd1
Leandro Cunha <leandroscunha@gmail.com>
2023-08-21 22:35:10 +0200

    feat: enhance CSS for the desktop version

--- src/components/ArticlePreview/ArticlePreview.jsx
+++ src/components/ArticlePreview/ArticlePreview.jsx
@@ -21,28 +21,26 @@ export const ArticlePreview = ({ posts }) => {
         {posts.map(post => {
           return (
             <li className={getBlocksWithArticleItem()} key={post.id}>
-              <div className={getBlocksWithArticleItem('__featured-image')}>
-                <GatsbyImage image={getImage(post.featuredImage)} alt={post.title.title} />
-              </div>
-              <div className={getBlocksWithArticleItem('__content')}>
-                <p className={getBlocksWithArticleItem('__content__category')}>{post.category}</p>
-                <Link
-                  to={`/blog/${post.id}`}
-                  className={getBlocksWithArticleItem('__content__link')}
-                >
+              <Link to={`/blog/${post.id}`} className={getBlocksWithArticleItem('__content__link')}>
+                <div className={getBlocksWithArticleItem('__featured-image')}>
+                  <GatsbyImage image={getImage(post.featuredImage)} alt={post.title.title} />
+                </div>
+                <div className={getBlocksWithArticleItem('__content')}>
+                  <p className={getBlocksWithArticleItem('__content__category')}>{post.category}</p>
+
                   <h2 className={getBlocksWithArticleItem('__content__title')}>
                     {post.title.title}
                   </h2>
-                </Link>
-                {post.description?.raw && <div>{renderRichText(post.description)}</div>}
-                <div className={getBlocksWithArticleItem('__content__meta')}>
-                  <span className="meta">{post.publishDate}</span>
+                  {post.description?.raw && <div>{renderRichText(post.description)}</div>}
+                  <div className={getBlocksWithArticleItem('__content__meta')}>
+                    <span className="meta">{post.publishDate}</span>
+                  </div>
+                  <p className={getBlocksWithArticleItem('__content__excerpt')}>
+                    {post.leadParagraph.leadParagraph}
+                  </p>
+                  <ArticleAuthor authorName={post.author} />
                 </div>
-                <p className={getBlocksWithArticleItem('__content__excerpt')}>
-                  {post.leadParagraph.leadParagraph}
-                </p>
-                <ArticleAuthor authorName={post.author} />
-              </div>
+              </Link>
             </li>
           );
         })}
