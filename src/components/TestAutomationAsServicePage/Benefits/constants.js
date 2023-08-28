6917ccbcf48770d8e7e49917eec08fc276694285
OleksandrDragun <42969419+OleksandrDragun@users.noreply.github.com>
2023-08-28 15:34:36 +0300

    implement test automation as a service page (#381)
    * implement test automation as a service page
    
    * remove redundant box shadow from benefit item block

---
+++ src/components/TestAutomationAsServicePage/Benefits/constants.js
@@ -0,0 +1,25 @@
+export const FLEXIBILITY_LIST = [
+  { info: 'Ability to spike capacity to accelerate Automation efforts' },
+  { info: 'Ability to add specific skills to the project without any contract changes' },
+  { info: 'Pay only for what you use through outcome-based model and Flex teams' },
+];
+
+export const LOWER_COST_LIST = [
+  { info: 'Reduce testing costs without reducing quality (up to 30% in savings)' },
+  { info: 'Cost-advantaged to traditional T&M, with exceptionally high standards for quality' },
+  { info: 'Continued high-quality testing from EPAM employees' },
+];
+
+export const FASTER_TTM_LIST = [
+  { info: 'Ensure faster provisioning of services' },
+  { info: 'Free up your teams for critical internal work' },
+  {
+    info: 'Leverage EPAM’s proven accelerators (e.g., Automation frameworks, reporting portals, sourcing technologies)',
+  },
+];
+
+export const SERVICE_LIST = [
+  { info: 'Ability to spike capacity to accelerate Automation efforts' },
+  { info: 'Ability to add specific skills to the project without any contract changes' },
+  { info: 'Pay only for what you use through outcome-based model and Flex teams' },
+];
