5c5137d6cd3041f17153a2cd9b5bf017b1375807
Oleksandr Dragun <oleksandr_dragun@epam.com>
2023-08-31 13:05:21 +0300

    implement requests block with simple horizontal scroll instead of marquee animation

--- src/components/QualityAssessmentPage/CommonRequests/CommonRequests.jsx
+++ src/components/QualityAssessmentPage/CommonRequests/CommonRequests.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Marquee from 'react-fast-marquee';
 
 import { Heading } from '../../Heading';
 import { TOP_REQUEST_LIST, BOTTOM_REQUEST_LIST } from './constants';
@@ -9,37 +8,23 @@ import './CommonRequests.scss';
 export const CommonRequests = () => (
   <div className="common-requests">
     <Heading title="Common client requests" />
-    <div className="common-requests__desktop-block requests">
-      <Marquee className="requests__marquee" speed={15} gradientWidth="19.27%">
+    <div className="requests__wrapper">
+      <div className="requests">
         {TOP_REQUEST_LIST.map(({ description, groupName }, index) => (
-          <div className="requests__marquee-item" key={index}>
-            <div className="requests__marquee-item-description">{description}</div>
-            <div className="requests__marquee-item-groupName">{groupName}</div>
+          <div className="requests__item" key={index}>
+            <div className="requests__item-description">{description}</div>
+            <div className="requests__item-group-name">{groupName}</div>
           </div>
         ))}
-      </Marquee>
-      <Marquee
-        className="requests__marquee requests__marquee--bottom"
-        speed={15}
-        gradientWidth="19.27%"
-      >
+      </div>
+      <div className="requests">
         {BOTTOM_REQUEST_LIST.map(({ description, groupName }, index) => (
-          <div className="requests__marquee-item" key={index}>
-            <div className="requests__marquee-item-description">{description}</div>
-            <div className="requests__marquee-item-group-name">{groupName}</div>
+          <div className="requests__item" key={index}>
+            <div className="requests__item-description">{description}</div>
+            <div className="requests__item-group-name">{groupName}</div>
           </div>
         ))}
-      </Marquee>
-    </div>
-    <div className="common-requests__mobile-block requests">
-      <Marquee className="requests__marquee" speed={15} gradientWidth="19.27%">
-        {[...TOP_REQUEST_LIST, ...BOTTOM_REQUEST_LIST].map(({ description, groupName }, index) => (
-          <div className="requests__marquee-item" key={index}>
-            <div className="requests__marquee-item-description">{description}</div>
-            <div className="requests__marquee-item-groupName">{groupName}</div>
-          </div>
-        ))}
-      </Marquee>
+      </div>
     </div>
   </div>
 );
