3e12a7991b0b6ba7c1862d49781ff7508e1db5aa
Viktor Soroka <viktor_soroka@epam.com>
2023-09-22 12:57:00 +0300

    Finalise case studies

--- src/templates/case-study.jsx
+++ src/templates/case-study.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react';
 import { graphql } from 'gatsby';
-import { noop } from 'lodash';
+import noop from 'lodash/noop';
+import compact from 'lodash/compact';
 import cx from 'classnames';
 import get from 'lodash/get';
 import { renderRichText } from 'gatsby-source-contentful/rich-text';
 import { BLOCKS } from '@contentful/rich-text-types';
 
+import DownloadIcon from '../svg/download.inline.svg';
 import { Layout } from '../components/Layout';
 import { Link } from '../components/Link';
 import { createBemBlockBuilder } from '../utils';
 import { SubscriptionBanner } from '../components/SubscriptionBanner';
 import { ArrowLeft } from '../svg/arrowLeft';
-import { Download } from '../svg/download';
 
 import './case-study.scss';
 
@@ -20,24 +21,24 @@ const getBlocksWith = createBemBlockBuilder(['case-page']);
 const CaseStudyTemplate = props => {
   useEffect(() => {
     function setupTable() {
-      const tables = document.querySelectorAll('table');
+      const tables = Array.from(document.querySelectorAll('table')).reverse();
+
       if (!tables.length) return;
 
       if (window.matchMedia('(min-width: 768px)').matches) {
-        tables[tables.length - 1].style.bottom = '8px';
-        tables.forEach((table, idx) => {
-          if (idx === tables.length - 1) return;
-          const bottom = (tables.length - (idx + 1)) * 46 + 18;
-          table.style.bottom = `${bottom}px`;
-        });
-      }
+        let counter = 0;
 
-      if (window.matchMedia('(min-width: 1239px)').matches) {
-        tables[tables.length - 1].style.bottom = '22px';
         tables.forEach((table, idx) => {
-          if (idx === tables.length - 1) return;
-          const bottom = (tables.length - (idx + 1)) * 70 + 18;
-          table.style.bottom = `${bottom}px`;
+          const ulPrevHeight = table.previousElementSibling.getBoundingClientRect().height;
+
+          table.style.height = `${ulPrevHeight}px`;
+          table.style.bottom = idx === 0 ? '0px' : `${counter}px`;
+
+          counter += ulPrevHeight;
+        });
+      } else {
+        tables.forEach(table => {
+          table.style.height = 'initial';
         });
       }
     }
@@ -53,7 +54,12 @@ const CaseStudyTemplate = props => {
   const options = {
     renderNode: {
       [BLOCKS.PARAGRAPH]: (node, children) => {
-        if (!children) return null;
+        const childrenArray = React.Children.toArray(children);
+
+        if (!compact(childrenArray).length) {
+          return null;
+        }
+
         return <p className={getBlocksWith('__column--description')}>{children}</p>;
       },
       [BLOCKS.LIST_ITEM]: (node, children) => {
@@ -84,53 +90,53 @@ const CaseStudyTemplate = props => {
   return (
     <Layout>
       <div className={getBlocksWith()}>
-        <div className={getBlocksWith('__head')}>
-          <p className={getBlocksWith('__industry')}>{industry}</p>
-          <h1 className={getBlocksWith('__title')}>{title}</h1>
-          <div className={getBlocksWith('__button-group')}>
-            <Link
-              className={cx('btn btn--white btn--large', getBlocksWith('__back-to-list'))}
-              to={'/case-studies/'}
-            >
-              <ArrowLeft />
-              Back to Case Studies
-            </Link>
-            <Link
-              className={cx('btn btn--white btn--large', getBlocksWith('__download-pdf'))}
-              onClick={noop}
-            >
-              <Download />
-              Download PDF version
-            </Link>
+        <div className={getBlocksWith('__hero')}>
+          <div className={cx(getBlocksWith('__head'), 'container')}>
+            <div className={getBlocksWith('__titles')}>
+              <p className={getBlocksWith('__industry')}>{industry}</p>
+              <h1 className={getBlocksWith('__title')}>{title}</h1>
+            </div>
+            <div className={getBlocksWith('__button-group')}>
+              <Link
+                className={cx('btn btn--white btn--large', getBlocksWith('__back-to-list'))}
+                to="/case-studies"
+              >
+                <ArrowLeft />
+                Back to Case Studies
+              </Link>
+              <a
+                className={cx(
+                  'btn btn--white btn--large temporary-hide',
+                  getBlocksWith('__download-pdf'),
+                )}
+                download
+                onClick={noop}
+              >
+                <DownloadIcon />
+                Download PDF version
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className={getBlocksWith('__case-body')}>
-        <div className={getBlocksWith('__case-columns')}>
-          <div className={getBlocksWith('__challenges')}>
-            <div className={getBlocksWith('__challenges--body')}>
-              {challenges?.raw && renderRichText(challenges, options)}
-            </div>
-            <div className={cx(getBlocksWith('__download-btn'), 'temporary-hide')}>
-              <button className={cx('btn', 'btn--primary', 'btn--large')} onClick={noop}>
-                <Download />
-                Download PDF version
-              </button>
-            </div>
-          </div>
-
-          <div className={getBlocksWith('__highlights')}>
-            <div className={getBlocksWith('__highlights--body')}>
-              {highlights?.raw && renderRichText(highlights, options)}
-            </div>
-            {benefitsResults?.raw && (
-              <div className={getBlocksWith('__benefitsResults')}>
-                <div className={getBlocksWith('__benefitsResults--body')}>
-                  {renderRichText(benefitsResults, options)}
-                </div>
+        <div className={cx(getBlocksWith('__case-body'), 'container')}>
+          <div className={getBlocksWith('__case-columns')}>
+            <div className={getBlocksWith('__challenges')}>
+              <div className={getBlocksWith('__challenges--body')}>
+                {challenges?.raw && renderRichText(challenges, options)}
               </div>
-            )}
+            </div>
+            <div className={getBlocksWith('__highlights')}>
+              <div className={getBlocksWith('__highlights--body')}>
+                {highlights?.raw && renderRichText(highlights, options)}
+              </div>
+              {benefitsResults?.raw && (
+                <div className={getBlocksWith('__benefitsResults')}>
+                  <div className={getBlocksWith('__benefitsResults--body')}>
+                    {renderRichText(benefitsResults, options)}
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
