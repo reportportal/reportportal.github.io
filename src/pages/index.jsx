4495a4c8e622b8ed89a9692f27a84ce351c4fa09
Vadzim Hvazdovich <vadzim_hvazdovich@epam.com>
2023-09-29 18:53:34 +0400

    GDPR add

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,4 +11,23 @@ const Root = () => {
   );
 };
 
+export function Head() {
+  return (
+    <>
+      <script
+        type="text/javascript"
+        src="https://cookie-cdn.cookiepro.com/consent/77055ecd-ec2c-461a-bf1c-3e84d715e668/OtAutoBlock.js"
+        defer
+      />
+      <script
+        type="text/javascript"
+        src="https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js"
+        defer
+        data-domain-script="77055ecd-ec2c-461a-bf1c-3e84d715e668"
+      />
+      <script type="text/javascript">{'function OptanonWrapper() {}'}</script>
+    </>
+  );
+}
+
 export default Root;
