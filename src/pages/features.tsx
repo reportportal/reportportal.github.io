3e64ed26023df24e8a9b1d3570ab4f3a56bce41a
Leandro Cunha <leandroscunha@gmail.com>
2023-10-06 01:56:34 +0200

    feat: migrate Features page to TS

---
+++ src/pages/features.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from '@/components/Layout';
+import { FeaturesPage } from '@/containers/FeaturesPage';
+
+export const Features: React.FC = () => (
+  <Layout className="features-page-layout">
+    <FeaturesPage />
+  </Layout>
+);
+
