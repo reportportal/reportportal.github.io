0066e28f57bb3328bcaa37f5c58fcd4625df47ad
Leandro Cunha <leandroscunha@gmail.com>
2023-10-06 02:05:38 +0200

    feat: migrate Installation page to TS

---
+++ src/pages/installation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from '@/components/Layout';
+import { InstallationPage } from '@/containers/InstallationPage';
+
+export const Installation: React.FC = () => (
+  <Layout>
+    <InstallationPage />
+  </Layout>
+);
+
