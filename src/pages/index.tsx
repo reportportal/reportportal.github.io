f6c28cad38cbcbe25e0bd595ef75f8b42fc9b5e7
Leandro Cunha <leandroscunha@gmail.com>
2023-10-06 02:01:46 +0200

    feat: migrate Pages root to TS

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from '@/components/Layout';
+import { LandingPage } from '@/containers/LandingPage';
+
+export const Root: React.FC = () => (
+  <Layout>
+    <LandingPage />
+  </Layout>
+);
+
