a1a4bb3eb85cc109b11f22c743d54d74afa0580b
Vadzim Hvazdovich <vadzim_hvazdovich@epam.com>
2023-10-06 10:14:24 +0400

    EPMRPP-86795 || add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
       'Centralized test automation dashboard. Provides AI-based defects triage and real time test report dashboard.',
     keywords:
       'test automation dashboard, test automation reporting, qa automation dashboard, test automation results dashboard, test report dashboard, qa metrics dashboard, test execution report, end to end testing reporting tools, ReportPortal installation, ReportPortal integration, ReportPortal dashboard',
+    url: 'https://reportportal.io/',
   },
   plugins: [
     'gatsby-plugin-svgr-svgo',
@@ -29,6 +30,46 @@ module.exports = {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
         id: process.env.GTM_ID,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: '/sitemap-landing',
+        query: `{
+          site {
+            siteMetadata {
+              url
+            }
+          }
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+        }`,
+        resolveSiteUrl: ({ site }) => {
+          return site.siteMetadata.url;
+        },
+        resolvePages: ({ allSitePage: { nodes: allPages } }) => {
+          return allPages.filter(page =>
+            [
+              '/contact-us/saas/',
+              '/contact-us/on-premises/',
+              '/contact-us/taas',
+              '/contact-us/taaas',
+              '/contact-us/qaaas',
+              '/contact-us/qasp',
+              '/contact-us/d4j',
+              '/contact-us/hlm',
+            ].every(path => !page.path.includes(path)),
+          );
+        },
+        serialize: ({ path }) => {
+          return {
+            url: path,
+          };
+        },
       },
     },
   ],
