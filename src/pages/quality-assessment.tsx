cdd69cface39236ce8a709f356f01ec4f2fd0caa
Leandro Cunha <leandroscunha@gmail.com>
2023-10-06 02:09:00 +0200

    feat: migrate Quality Assesssment page to TS

---
+++ src/pages/quality-assessment.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from '@/components/Layout';
+import { QualityAssessmentPage } from '@/containers/QualityAssessmentPage';
+
+export const TestAutomationAsService: React.FC = () => (
+  <Layout>
+    <QualityAssessmentPage />
+  </Layout>
+);
+
