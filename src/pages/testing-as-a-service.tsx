5ce9c0e95f0662f2d3638313ce395f7ba08b3413
Leandro Cunha <leandroscunha@gmail.com>
2023-10-06 02:15:37 +0200

    feat: migrate Testing page to TS

---
+++ src/pages/testing-as-a-service.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from '@/components/Layout';
+import { TestingAsServicePage } from '@/containers/TestingAsServicePage';
+
+export const TestingAsService: React.FC = () => (
+  <Layout>
+    <TestingAsServicePage />
+  </Layout>
+);
+
