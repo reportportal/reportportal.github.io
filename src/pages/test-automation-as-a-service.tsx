932d79b051f3b0d1923e68817f9deef19452f184
Leandro Cunha <leandroscunha@gmail.com>
2023-10-06 02:11:58 +0200

    feat: migrate TestAutomation page to TS

---
+++ src/pages/test-automation-as-a-service.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from '@/components/Layout';
+import { TestAutomationAsServicePage } from '@/containers/TestAutomationAsServicePage';
+
+export const TestAutomationAsService: React.FC = () => (
+  <Layout>
+    <TestAutomationAsServicePage />
+  </Layout>
+);
+
