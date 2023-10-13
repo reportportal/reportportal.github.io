d8ddde160459785713dc0e97c5e52b3436395c3b
Leandro Cunha <leandroscunha@gmail.com>
2023-10-13 23:08:36 +0200

    feat: migrate ArrowLink component to TS

--- src/components/ArrowLink/ArrowLink.tsx
+++ src/components/ArrowLink/ArrowLink.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import { createBemBlockBuilder } from '@utils';
+import { createBemBlockBuilder } from '../../utils';
 
 import { Link } from '../Link';
 import ArrowIcon from './icons/arrow-icon.inline.svg';
+
+interface Props {
+  text: string
+  mode: string
+}
 
 const getBlocksWith = createBemBlockBuilder(['arrow-link']);
 
 import './ArrowLink.scss';
 
-export const ArrowLink = ({ text, mode, ...rest }) => (
+export const ArrowLink: React.FC<Props> = ({ text, mode, ...rest }) => (
   <Link {...rest} className={classNames(getBlocksWith(), getBlocksWith(`--${mode}`))}>
     {text}
     <ArrowIcon />
