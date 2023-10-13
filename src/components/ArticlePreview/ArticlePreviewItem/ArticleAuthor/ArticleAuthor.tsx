0420fa1a0726450db21bd54e69ad2ad7e138210d
Leandro Cunha <leandroscunha@gmail.com>
2023-10-13 23:23:49 +0200

    feat: migrate ArticlePreview component to TS

--- src/components/ArticlePreview/ArticlePreviewItem/ArticleAuthor/ArticleAuthor.tsx
+++ src/components/ArticlePreview/ArticlePreviewItem/ArticleAuthor/ArticleAuthor.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import { createBemBlockBuilder } from '@utils';
+import { createBemBlockBuilder } from '../../../../utils';
 
 import AuthorIcon from './icons/author.inline.svg';
 
 import './ArticleAuthor.scss';
 
+interface Props {
+  authorName: string
+}
+
 const getBlocksWith = createBemBlockBuilder(['article-author']);
 
-export const ArticleAuthor = ({ authorName }) => {
+export const ArticleAuthor: React.FC<Props> = ({ authorName }) => {
   return (
     <p className={getBlocksWith()}>
       <span className={getBlocksWith('__icon')}>
