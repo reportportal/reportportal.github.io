f5ab8bcae5d8db8f286c6f1d375d677ff2ef74b2
Leandro Cunha <leandroscunha@gmail.com>
2023-10-15 13:16:07 +0200

    feat: migrate Layout component to TS

---
+++ src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { Divider } from 'antd';
+import classNames from 'classnames';
+
+import { createBemBlockBuilder } from '../../../utils';
+
+import { Link } from '../../Link';
+import { FooterList } from './FooterList';
+import { PRODUCT_LINKS, SERVICES_LINKS, SOLUTIONS_LINKS, LEARN_LINKS } from './constants';
+import TwitterIcon from './icons/twitter.inline.svg';
+import YoutubeIcon from './icons/youtube.inline.svg';
+import GithubIcon from './icons/github.inline.svg';
+import SlackIcon from './icons/slack.inline.svg';
+import LinkedinIcon from './icons/linkedin.inline.svg';
+import LambdaIcon from './icons/lambda.inline.svg';
+import { NavLogoIcon } from './icons';
+
+import './Footer.scss';
+
+const getBlocksWith = createBemBlockBuilder(['footer']);
+
+const socialLinks = [
+  {
+    link: 'https://slack.epmrpp.reportportal.io/',
+    icon: <SlackIcon />,
+  },
+  {
+    link: 'https://twitter.com/reportportal_io',
+    icon: <TwitterIcon />,
+  },
+  {
+    link: 'https://www.linkedin.com/company/reportportal/',
+    icon: <LinkedinIcon />,
+  },
+  {
+    link: 'https://www.youtube.com/c/ReportPortal',
+    icon: <YoutubeIcon />,
+  },
+  {
+    link: 'https://github.com/reportportal/reportportal',
+    icon: <GithubIcon />,
+  },
+];
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className={getBlocksWith()}>
+      <div className={classNames(getBlocksWith('__container'), 'container')}>
+        <section className={getBlocksWith('__navigation')}>
+          <div className={getBlocksWith('__purpose')}>
+            <Link to="/" className={getBlocksWith('__logo')}>
+              <NavLogoIcon />
+            </Link>
+            <span>
+              ReportPortal is a service, that provides increased capabilities to speed up results
+              analysis and reporting through the use of built-in analytics features.
+            </span>
+            <span>© {new Date().getFullYear()} ReportPortal</span>
+          </div>
+          <div className={getBlocksWith('__navigation-links')}>
+            <FooterList title="PRODUCT" items={PRODUCT_LINKS} />
+            <FooterList title="SERVICES" items={SERVICES_LINKS} />
+            <FooterList title="SOLUTIONS" items={SOLUTIONS_LINKS} />
+            <FooterList title="LEARN" items={LEARN_LINKS} />
+          </div>
+        </section>
+        <Divider />
+        <section className={getBlocksWith('__secondary')}>
+          <div className={getBlocksWith('__legal')}>
+            <span className={getBlocksWith('__license')}>
+              <span>
+                Sponsored by&nbsp;
+                <Link to="https://www.epam.com/">EPAM</Link>
+              </span>
+              <span>Licensed under Apache v2.0</span>
+            </span>
+            <div className={getBlocksWith('__terms')}>
+              <Link to="/legal/terms">Terms & Conditions</Link>
+              <Link to="https://privacy.epam.com/core/interaction/showpolicy?type=PrivacyPolicy">
+                Privacy Policy
+              </Link>
+            </div>
+          </div>
+          <div className={getBlocksWith('__other-links')}>
+            <ul className={getBlocksWith('__social-links')}>
+              {socialLinks.map((socialLink, index) => (
+                <li key={index}>
+                  <Link to={socialLink.link}>{socialLink.icon}</Link>
+                </li>
+              ))}
+            </ul>
+            <Link to="https://www.lambdatest.com/">
+              <LambdaIcon />
+            </Link>
+          </div>
+        </section>
+      </div>
+    </footer>
+  );
+};
---
+++ src/components/Layout/Footer/constants.ts
@@ -0,0 +1,77 @@
+import { DOCUMENTATION_URL } from '../../../utils/constants';
+
+export const PRODUCT_LINKS = [
+  {
+    title: 'Installation',
+    link: '/installation',
+  },
+  {
+    title: 'Features',
+    link: '/features',
+  },
+  {
+    title: 'Release notes',
+    link: `${DOCUMENTATION_URL}/category/releases/`,
+  },
+  {
+    title: 'Integrations',
+    link: `${DOCUMENTATION_URL}/category/plugins/`,
+  },
+  {
+    title: 'Pricing',
+    link: '/pricing/on-premises',
+  },
+];
+
+export const SERVICES_LINKS = [
+  {
+    title: 'Testing as a Service',
+    link: '/testing-as-a-service',
+  },
+  {
+    title: 'Test Automation as a Service',
+    link: '/test-automation-as-a-service',
+  },
+  {
+    title: 'Quality Assessment',
+    link: '/quality-assessment',
+  },
+];
+
+export const SOLUTIONS_LINKS = [
+  {
+    title: 'TDSpora',
+    link: 'https://tdspora.ai/',
+  },
+  {
+    title: 'Drill4J',
+    link: 'https://drill4j.github.io/',
+  },
+  {
+    title: 'Healenium',
+    link: 'https://healenium.io/',
+  },
+  {
+    title: 'QaSpace',
+    link: 'https://marketplace.atlassian.com/apps/1214038/qaspace-test-management?tab=overview&hosting=server',
+  },
+];
+
+export const LEARN_LINKS = [
+  {
+    title: 'Documentation',
+    link: DOCUMENTATION_URL,
+  },
+  {
+    title: 'Blog',
+    link: '/blog',
+  },
+  {
+    title: 'Case Studies',
+    link: '/case-studies',
+  },
+  {
+    title: 'FAQ',
+    link: `${DOCUMENTATION_URL}/FAQ/`,
+  },
+];
