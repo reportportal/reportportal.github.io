b54630b1755529288184012ca5ff17ebd17055f2
Leandro Cunha <leandroscunha@gmail.com>
2023-10-15 12:04:40 +0200

    feat: migrate Heading component to TS

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './Heading.scss';
+
+interface Props {
+  title: string
+  color: string
+  tag: keyof JSX.IntrinsicElements;
+}
+
+const headingSize = {
+  h2: 'secondary-heading',
+  h3: 'third-heading',
+};
+
+export const Heading: React.FC<Props> = ({ title, color = 'black-heading', tag: Tag = 'h2' }) => (
+  <Tag className={classNames('heading', headingSize[Tag as keyof typeof headingSize], color)}>{title}</Tag>
+);
