065bab5f07c398b751cc594d9878742b75dfe9d0
Leandro Cunha <leandroscunha@gmail.com>
2023-10-15 20:07:16 +0200

    feat: migrate SubscriptionForm component to TS

--- src/components/SubscriptionForm/icons/Envelope.tsx
+++ src/components/SubscriptionForm/icons/Envelope.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const EnvelopeIcon = () => {
+export const EnvelopeIcon: React.FC = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
       <path
