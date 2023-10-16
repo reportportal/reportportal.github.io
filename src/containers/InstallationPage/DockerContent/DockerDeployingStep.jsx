408f6d5cc536a3f2a8d360c3be8f25a291fd0ae2
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2023-10-17 02:34:07 +0300

    Update docker installation guide

--- src/containers/InstallationPage/DockerContent/DockerDeployingStep.jsx
+++ src/containers/InstallationPage/DockerContent/DockerDeployingStep.jsx
@@ -47,70 +47,29 @@ export const DockerDeployingStep = () => {
           https://raw.githubusercontent.com/reportportal/reportportal/master/docker-compose.yml
         </Text>
 
-        <p>2. Make the ElasticSearch configuration prerequisites for the analyzer service:</p>
-        <ul className={getBlocksWith('__marked-list')}>
-          <li>
-            Set <span className={getBlocksWith('__code-text')}>vm.max_map_count</span> kernel
-            setting before ReportPortal deploying with the following{' '}
-            <Link
-              className={getBlocksWith('__link')}
-              to="https://www.elastic.co/guide/en/elasticsearch/reference/7.10/docker.html#docker-cli-run-prod-mode"
-            >
-              Commands
-            </Link>{' '}
-            <span className={getBlocksWith('__sub-text')}>(optional)</span>
-          </li>
-          <li>Give right permissions to ElasticSearch data folder using the following commands:</li>
-        </ul>
-
-        <Text className={getBlocksWith('__code')} code copyable>
-          {
-            'mkdir -p ./data/elasticsearch\nchmod 775 ./data/elasticsearch\nchgrp 1000 ./data/elasticsearch'
-          }
-        </Text>
-
         <p>
-          For more details about ElasticSearch installation with Docker visit
-          <Link
-            className={getBlocksWith('__link')}
-            to="https://www.elastic.co/guide/en/elasticsearch/reference/7.10/docker.html#_notes_for_production_use_and_defaults"
-          >
-            {' '}
-            guide
-          </Link>
-          .
-        </p>
-
-        <p>
-          3. PostgreSQL Performance Tuning{' '}
-          <span className={getBlocksWith('__sub-text')}>(optional)</span>
-        </p>
-        <p>
-          Depends on your hardware configuration and parameters of your system, you can additionally
-          optimize your PostgreSQL performance by adding the following parameters to {'"command"'}{' '}
-          option in the Docker compose file.
-        </p>
-        <p>
-          Please choose the set of the values of these variables that are right for your system:
+          2. Ensure you override the UAT Service environment variable{' '}
+          <span className={getBlocksWith('__code-text')}>RP_INITIAL_ADMIN_PASSWORD</span>
         </p>
 
         <Text className={getBlocksWith('__code')} code copyable>
-          {
-            '-c effective_io_concurrency=\n-c shared_buffers=\n-c max_connections=\n-c effective_cache_size=\n-c maintenance_work_mem=\n-c random_page_cost=\n-c seq_page_cost=\n-c min_wal_size=\n-c max_wal_size=\n-c max_worker_processes=\n-c max_parallel_workers_per_gather=\n'
-          }
+          {`
+version: '2.4'
+services:
+  uat:
+    environment:
+      RP_INITIAL_ADMIN_PASSWORD: "My_Strong_Password!"
+            `}
         </Text>
 
         <p>
-          You can change PostgreSQL password by editing environment variables in{' '}
-          <Link
-            className={getBlocksWith('__link')}
-            to="https://github.com/reportportal/reportportal/blob/master/docker-compose.yml"
-          >
-            docker-compose.yml
-          </Link>
-          .
+          {`We've modified the current approach: during the initial installation and the first login
+          of the superadmin, they will need to create a unique initial password, different from the
+          default password provided in the ReportPortal installation documentation. Failure to do so
+          will prevent the Auth service from starting.`}
         </p>
-        <p>4. Start the application using the following command:</p>
+
+        <p>3. Start the application using the following command:</p>
 
         <Text className={getBlocksWith('__code')} code copyable>
           docker-compose -p reportportal up -d --force-recreate
@@ -132,19 +91,100 @@ export const DockerDeployingStep = () => {
           <span className={getBlocksWith('__code-text')}>--force-recreate</span> — re-creates
           containers
         </p>
-      </div>
 
-      <Notice title="Useful commands">
-        <span className={getBlocksWith('__code-text')}>docker-compose logs</span> — shows logs from
-        all containers <br />
-        <span className={getBlocksWith('__code-text')}>docker logs &lt;container_name&gt;</span> —
-        shows logs from selected container <br />
-        <p className={getBlocksWith('__code-text')}>
-          docker ps -a | grep {'"reportportal_"'} | awk {"'{print $1}'"} | xargs <br />
-          docker rm -f
-        </p>{' '}
-        <br />— deletes all ReportPortal containers
-      </Notice>
+        <Notice title="Useful commands">
+          <span className={getBlocksWith('__code-text')}>docker-compose logs</span> — shows logs
+          from all containers <br />
+          <span className={getBlocksWith('__code-text')}>docker logs &lt;container_name&gt;</span> —
+          shows logs from selected container <br />
+          <p className={getBlocksWith('__code-text')}>
+            docker ps -a | grep {'"reportportal_"'} | awk {"'{print $1}'"} | xargs <br />
+            docker rm -f
+          </p>{' '}
+          <br />— deletes all ReportPortal containers
+        </Notice>
+
+        <p>
+          3. Expose Docker Volumes to the file system{' '}
+          <span className={getBlocksWith('__sub-text')}>(optional)</span>
+        </p>
+        <ul className={getBlocksWith('__marked-list')}>
+          <li>
+            Set <span className={getBlocksWith('__code-text')}>vm.max_map_count</span> kernel
+            setting before ReportPortal deploying with the following{' '}
+            <Link
+              className={getBlocksWith('__link')}
+              to="https://www.elastic.co/guide/en/elasticsearch/reference/7.10/docker.html#docker-cli-run-prod-mode"
+            >
+              Commands
+            </Link>
+          </li>
+          .
+          <li>Give right permissions to ElasticSearch data folder using the following commands:</li>
+        </ul>
+
+        <Text className={getBlocksWith('__code')} code copyable>
+          {
+            'mkdir -p ./data/elasticsearch\nchmod 777 ./data/elasticsearch\nchgrp 1000 ./data/elasticsearch'
+          }
+        </Text>
+
+        <p>
+          For more details about ElasticSearch installation with Docker visit
+          <Link
+            className={getBlocksWith('__link')}
+            to="https://www.elastic.co/guide/en/elasticsearch/reference/7.10/docker.html#_notes_for_production_use_and_defaults"
+          >
+            {' '}
+            guide
+          </Link>
+          .
+        </p>
+
+        <p>
+          4. PostgreSQL Performance Tuning{' '}
+          <span className={getBlocksWith('__sub-text')}>(optional)</span>
+        </p>
+        <p>
+          Depends on your hardware configuration and parameters of your system, you can additionally
+          optimize your PostgreSQL performance by adding the following parameters to {'"command"'}{' '}
+          option in the Docker compose file.
+        </p>
+        <p>
+          Please choose the set of the values of these variables that are right for your system:
+        </p>
+
+        <Text className={getBlocksWith('__code')} code copyable>
+          {
+            '-c effective_io_concurrency=\n-c shared_buffers=\n-c max_connections=\n-c effective_cache_size=\n-c maintenance_work_mem=\n-c random_page_cost=\n-c seq_page_cost=\n-c min_wal_size=\n-c max_wal_size=\n-c max_worker_processes=\n-c max_parallel_workers_per_gather=\n'
+          }
+        </Text>
+
+        <p>
+          Please choose to set the values of these variables that are right for your system. You can
+          also change the PostgreSQL host by passing a new value to the{' '}
+          <span className={getBlocksWith('__code-text')}>POSTGRES_SERVER</span>
+          environment{' '}
+          <Link
+            className={getBlocksWith('__link')}
+            to={`${DOCUMENTATION_URL}/installation-steps/AdditionalConfigurationParameters`}
+          >
+            variable
+          </Link>
+          .
+        </p>
+
+        <p>
+          More info can be found at the following{' '}
+          <Link
+            className={getBlocksWith('__link')}
+            to={`${DOCUMENTATION_URL}/installation-steps/OptimalPerformanceHardwareSetup#5-postgresql-performance-tuning`}
+          >
+            link
+          </Link>
+          .
+        </p>
+      </div>
     </>
   );
 };
