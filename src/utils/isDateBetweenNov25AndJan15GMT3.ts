4988b6a845c67cd88bcdc7858df248c00c0739f5
Viktor Soroka <viktor_soroka@epam.com>
2024-01-12 13:24:40 +0200

    Adjust new year mode

--- src/utils/isDateBetweenNov25AndJan15GMT3.ts
+++ src/utils/isDateBetweenNov25AndJan15GMT3.ts
@@ -3,7 +3,7 @@ export const isDateBetweenNov25AndJan15GMT3 = () => {
 
   const isNovemberOrLater = currentDate.getUTCMonth() >= 10;
   const startDate = new Date(Date.UTC(currentDate.getUTCFullYear(), 10, 24, 21, 0, 0));
-  const endDate = new Date(Date.UTC(currentDate.getUTCFullYear(), 0, 4, 21, 0, 0));
+  const endDate = new Date(Date.UTC(currentDate.getUTCFullYear(), 0, 14, 21, 0, 0));
 
   const result = isNovemberOrLater ? currentDate >= startDate : currentDate <= endDate;
 
