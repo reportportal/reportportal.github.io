42d85c2123f0f6375caedb9f5dfdcc71429506b8
Ilya <ilya_hancharyk@epam.com>
2025-05-20 12:05:36 +0200

    Add cookie-pro scripts

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
+    <script
+      key="OtAutoBlock"
+      type="text/javascript"
+      defer
+      src="https://cookie-cdn.cookiepro.com/consent/77055ecd-ec2c-461a-bf1c-3e84d715e668/OtAutoBlock.js"
+    />,
+    <script
+      key="otSDKStub"
+      type="text/javascript"
+      defer
+      src="https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js"
+      data-domain-script="77055ecd-ec2c-461a-bf1c-3e84d715e668"
+    />,
+    <script key="OptanonWrapper" type="text/javascript">
+      {'function OptanonWrapper() { }'}
+    </script>,
     <link
       key="visualwebsiteoptimizer"
       rel="preconnect"
