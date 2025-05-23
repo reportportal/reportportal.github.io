1808587e42a70e24eb51392fb8f01ef1e7943244
Alla Prischepa <38905429+allaprischepa@users.noreply.github.com>
2025-05-23 14:40:47 +0400

    EPMRPP-95893 || Add cookie scripts except the OtAutoBlock.js (#585)

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
+    <script
+      key="otSDKStub"
+      type="text/javascript"
+      defer
+      src="https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js"
+      data-domain-script="77055ecd-ec2c-461a-bf1c-3e84d715e668"
+    />,
+    <script key="OptanonWrapper" type="text/javascript">
+      {'function OptanonWrapper() { }'}
+    </script>,
     <link
       key="visualwebsiteoptimizer"
       rel="preconnect"
