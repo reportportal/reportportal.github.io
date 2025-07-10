1a17109c27cd48d92a574bb19c81c58a8e323d65
Viktor Soroka <viktor_soroka@epam.com>
2025-07-10 19:33:17 +0300

    Extend SEO trusted domains (#589)

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -13,13 +13,15 @@ export interface LinkProps extends HTMLAttributes<HTMLAnchorElement> {
 
 // These links are considered SEO trusted, they should be opened in the new tab without "rel" attribute set
 const TRUSTED_DOMAINS = [
+  'https://merch.reportportal.io',
+  'https://reportportal.io',
+  'https://demo.reportportal.io',
   'https://tdspora.ai',
   'https://drill4j.github.io',
   'https://healenium.io',
-  'https://merch.reportportal.io',
+  'https://mobitru.com',
+  'https://display.test.io',
   'https://console.cloud.google.com/marketplace/product/epam-mp-rp/reportportal?project=epam-mp-rp',
-  'https://reportportal.io',
-  'https://demo.reportportal.io',
   DOCUMENTATION_URL,
 ];
 
